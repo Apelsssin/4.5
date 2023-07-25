9a9e5d897a4687f3056c7c26c47775a95ff9c6e0
Zhitikhina Mariia <zhitikhinamv@gmail.com>
2023-07-25 22:59:07 +0300

    first commit

---
+++ 4.5.1/4.5.1.cpp
@@ -0,0 +1,99 @@
+﻿#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <any>
+#include <variant>
+#include <string>
+
+template <class T>
+T squaring(T n) {
+	n = n * n;
+	return n;
+}
+template <class T>
+std::vector<T> squaring(std::vector<T> v) {
+	for (T& it : v) {
+		it *= it;
+	}
+	return v;
+}
+template <class T>
+void print(std::vector<T> v) {
+	for (T& it : v) {
+		std::cout << it << ", ";
+	}
+	std::cout << std::endl;
+}
+
+int main() {
+	std::cout << "[IN]: ";
+	std::string str;
+	//std::cin >> str;
+	str = "-1, 4, 8"; //
+	std::cout << str << std::endl; //
+	bool doubl = false;
+	bool one = true;
+	size_t counter = 1;
+	for (int i = 0; i < str.length(); ++i) {
+		if (str[i] == 46) doubl = true; // "." - 46
+		if (str[i] == 32) { // " " - 32. 
+			one = false;
+			++counter;
+		}
+	}
+	std::cout << "[OUT]: ";
+	if (one && doubl) {
+		double n = std::atof(str.c_str());
+		std::cout << squaring(n);
+	}
+	else if (one && !doubl) {
+		int n = std::atoi(str.c_str());
+		std::cout << squaring(n);
+	}
+	else {
+		if (doubl) {
+			double* arr = new double[counter];
+			std::string s;
+			std::vector  <double> v;
+			v.resize(counter);
+			size_t j = 0;
+			for (size_t i = 0; i < str.length(); ++i) {
+				if (str[i] != 32 && str[i] != 44) {
+					s += str[i];
+				}
+				if (str[i] == 32) {
+					v[j] = std::stof(s);
+					++j;
+					s = "";
+				}
+				if (i == str.length() - 1)
+					v[j] = std::stof(s);
+			}
+
+			v = squaring(v);
+			print(v);
+		}
+		else {
+			int* arr = new int[counter];
+			std::string s;
+			std::vector  <int> v;
+			v.resize(counter);
+			size_t j = 0;
+			for (size_t i = 0; i < str.length(); ++i) {
+				if (str[i] != 32 && str[i] != 44) {
+					s += str[i];
+				}
+				if (str[i] == 32) {
+					v[j] = std::stoi(s);
+					++j;
+					s = "";
+				}
+				if (i == str.length() - 1) 
+					v[j] = std::stoi(s);
+			}
+
+			v = squaring(v);
+			print(v);
+		}
+	}
+}
---
+++ 4.5.3/4.5.3.cpp
@@ -0,0 +1,53 @@
+﻿
+#include <iostream>
+#include <vector>
+template <class T>
+class get_sum {
+    std::vector <T> v;
+    T n = 0;
+    int size;
+public:
+    get_sum(std::vector <T> v, int size) : v(v), size(size) { }
+    T operator() () {
+        for (int i = 0; i < size; ++i) {
+            if (v[i] % 3 == 0)
+                n += v[i];
+        }
+        return n;
+    }
+};
+
+template <class T>
+class get_count {
+    std::vector <T> v;
+    int m = 0;
+    int size;
+public:
+    get_count(std::vector <T> v, int size) : v(v), size(size) { }
+    int operator() () {
+        for (int i = 0; i < size; ++i) {
+            if (v[i] % 3 == 0)
+                ++m;
+        }
+        return m;
+    }
+};
+
+template <class T>
+void print (std::vector<T> v,int size) {
+    for (int it : v) {
+        std::cout << it << ", ";
+    }
+    std::cout << std::endl;
+}
+
+int main()
+{
+    std::vector v{ 4, 1, 3, 6, 25, 54 };
+    std::cout << "[IN] :  "; 
+    print(v, v.size()); 
+    get_sum x(v, v.size());
+    get_count y(v, v.size());
+    std::cout << "[OUT] : get_sum() = " << x() << std::endl; 
+    std::cout << "[OUT] : get_count() = " << y() << std::endl;
+}
